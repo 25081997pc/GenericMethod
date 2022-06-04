2f0dbfd0d2460fa592ee34d65b9ca23332d70917
25081997pc <chavanpooja2508@gmail.com>
2022-06-04 18:19:20 +0530

    Finding Maximum String

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -8,30 +8,30 @@ namespace Generic
 {
     public class FindMaximum
     {  
-          public double MaximumFloatNum(double firstValue, double secondValue, double thirdValue)
+          public string MaximumString(string firstString, string secondString, string thirdString)
           {
-                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                    firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                    firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+                if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                    firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                    firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
 
                 {     
-                    return firstValue;
+                    return firstString;
                 }
-                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
-                    secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
-                    secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+                if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                    secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                    secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
 
                 {    
-                    return secondValue;
+                    return secondString;
                 }
-                if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
-                   thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
-                   thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+                if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                   thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                   thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
 
                 {  
-                    return thirdValue;
+                    return thirdString;
                 }
-                return firstValue;
+                return firstString;
           }    
     }
 }
